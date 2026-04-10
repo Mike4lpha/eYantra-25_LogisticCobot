55d787ff8fb419c3f36e514a3a5c5ff463dbc298
Mike4lpha <155419677+Mike4lpha@users.noreply.github.com>
2026-04-11 01:12:03 +0530

    Delete build directory

--- build/ur_moveit_config/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mikealpha/colcon_ws/src/ur_moveit_config
-# Build directory: /home/mikealpha/colcon_ws/build/ur_moveit_config
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
